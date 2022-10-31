791f092ae5305e06cb92d0ba903b7d38e9028815
Francisco Pina Martins <stunts@pinamatins.com>
2022-10-31 14:56:58 +0000

    Adds the random seed and the random order names file

--- docs/scripts/order.R
+++ docs/scripts/order.R
@@ -1,12 +1,12 @@
 sorteio = function(names_file, seed) {
-    names = read.csv(names_file,
-                     header=FALSE)
-
-    set.seed(seed)
-
-    shuffled_names = as.data.frame(sample(t(names)))
-
-    return(shuffled_names)
+  names = read.csv(names_file,
+                   header=FALSE)
+  
+  set.seed(seed)
+  
+  shuffled_names = as.data.frame(sample(t(names)))
+  
+  return(shuffled_names)
 }
 
-print(sorteio("https://gitlab.com/StuntsPT/bp2022/raw/master/docs/classes/C01_assets/nomes.txt", 12345))
+print(sorteio("https://gitlab.com/StuntsPT/bp2022/raw/master/docs/classes/C01_assets/nomes.txt", 3728194))
